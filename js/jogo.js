d36beb93611ee6e0dbdf4c65d3e732c3202a7e7a
gerson-dev <gersondev@hotmail.com>
2020-06-10 16:31:53 -0300

    subindo todos os arquivos do jogo estoura balões

---
+++ js/jogo.js
@@ -0,0 +1,23 @@
+function addBola (){
+    var bola = document.createElement("div");
+    bola.setAttribute("class", "bola");
+    var p1 = Math.floor(Math.random() * 500);
+    var p2 = Math.floor(Math.random() * 400);
+    var cor = Math.floor(Math.random() * 999999 );
+    bola.setAttribute("style", "left:"+p1+"px;top:"+p2+"px;background-color:#"+cor+";");
+    bola.setAttribute("onclick", "estourar(this)");
+    document.body.appendChild(bola);
+   }
+   function estourar(elemento) {
+    document.body.removeChild(elemento);
+    var p = parseInt(document.getElementById("pontos").innerHTML);
+    p = p +1;
+    document.getElementById("pontos").innerHTML = p;
+    if (p > 100) {
+     alert ("PARABÉNS VOÇÊ GANHOU PIETRO E THOMAS");
+     window.location.href = window.location.href;
+    }
+    }
+   function iniciar (){
+    setInterval(addBola, 1000);
+   }
